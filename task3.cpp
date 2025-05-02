b51f88ed72db40c05c613a547c7f92c65fb9fa5b
VladDuplava <duplava.vladyslav@chnu.edu.ua>
2025-05-02 10:26:50 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -10,51 +10,59 @@ protected:
 public:
     Person(const string& n = "Невідомо") : name(n) {}
     virtual ~Person() {}
-    friend istream& operator>>(istream& is, Person& p) {
+
+    virtual istream& input(istream& is) {
         cout << "Введіть ім'я: ";
-        is >> p.name;
+        is >> name;
         return is;
     }
-    friend ostream& operator<<(ostream& os, const Person& p) {
-        os << "Ім'я: " << p.name;
+
+    virtual ostream& output(ostream& os) const {
+        os << "Ім'я: " << name;
         return os;
     }
 };
 
 // Похідний клас Батько
-class Father : public Person {
+class Father : virtual public Person {
 protected:
     string job;
 public:
     Father(const string& n = "Невідомо", const string& j = "Невідомо") : Person(n), job(j) {}
     virtual ~Father() {}
-    friend istream& operator>>(istream& is, Father& f) {
-        is >> (Person&)f;
+
+    istream& input(istream& is) override {
+        Person::input(is);
         cout << "Введіть професію: ";
-        is >> f.job;
+        is >> job;
         return is;
     }
-    friend ostream& operator<<(ostream& os, const Father& f) {
-        os << (Person&)f << ", Професія: " << f.job;
+
+    ostream& output(ostream& os) const override {
+        Person::output(os);
+        os << ", Професія: " << job;
         return os;
     }
 };
 
 // Похідний клас Мати
-class Mother : public Person {
+class Mother : virtual public Person {
 protected:
     string hobby;
 public:
     Mother(const string& n = "Невідомо", const string& h = "Невідомо") : Person(n), hobby(h) {}
     virtual ~Mother() {}
-    friend istream& operator>>(istream& is, Mother& m) {
-        is >> (Person&)m;
+
+    istream& input(istream& is) override {
+        Person::input(is);
         cout << "Введіть хобі: ";
-        is >> m.hobby;
+        is >> hobby;
         return is;
     }
-    friend ostream& operator<<(ostream& os, const Mother& m) {
-        os << (Person&)m << ", Хобі: " << m.hobby;
+
+    ostream& output(ostream& os) const override {
+        Person::output(os);
+        os << ", Хобі: " << hobby;
         return os;
     }
 };
@@ -66,27 +74,42 @@ private:
 public:
     Daughter(const string& n = "Невідомо", const string& j = "Невідомо",
              const string& h = "Невідомо", const string& s = "Невідомо")
-        : Father(n, j), Mother(n, h), school(s) {}
+        : Person(n), Father(n, j), Mother(n, h), school(s) {}
     virtual ~Daughter() {}
-    friend istream& operator>>(istream& is, Daughter& d) {
+
+    istream& input(istream& is) override {
         cout << "Для Батька:\n";
-        is >> (Father&)d;
+        Father::input(is);
         cout << "Для Матері:\n";
-        is >> (Mother&)d;
+        Mother::input(is);
         cout << "Введіть школу: ";
-        is >> d.school;
+        is >> school;
         return is;
     }
-    friend ostream& operator<<(ostream& os, const Daughter& d) {
-        os << "Дочка (з боку Батька): " << (Father&)d << ", (з боку Матері): " << (Mother&)d
-           << ", Школа: " << d.school;
+
+    ostream& output(ostream& os) const override {
+        os << "Дочка (з боку Батька): ";
+        Father::output(os);
+        os << ", (з боку Матері): ";
+        Mother::output(os);
+        os << ", Школа: " << school;
         return os;
     }
+
     void showInfo() const {
-        cout << "Інформація про Дочку: Ім'я: " << Father::name << ", Професія: " << job
+        cout << "Інформація про Дочку: Ім'я: " << name << ", Професія: " << job
              << ", Хобі: " << hobby << ", Школа: " << school << endl;
     }
 };
+
+// Перевантаження операторів для зручності
+istream& operator>>(istream& is, Person& p) {
+    return p.input(is);
+}
+
+ostream& operator<<(ostream& os, const Person& p) {
+    return p.output(os);
+}
 
 int main() {
     Daughter d;
